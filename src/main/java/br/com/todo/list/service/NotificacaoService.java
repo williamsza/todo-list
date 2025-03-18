69f6baa675fec28d6ac1303e7d90743191996934
williamsza <wyllsza@gmail.com>
2025-03-18 16:03:19 -0300

    Criada classe NotificacaoService, criado método verificarNotificação

---
+++ src/main/java/br/com/todo/list/service/NotificacaoService.java
@@ -0,0 +1,24 @@
+package br.com.todo.list.service;
+
+import br.com.todo.list.model.Tarefa;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class NotificacaoService {
+
+    public CompletableFuture<Void> verificarNotificacoes(List<Tarefa> tarefas) {
+        return CompletableFuture.runAsync(() -> {
+            for (Tarefa tarefa : tarefas) {
+                long diasRestantes = ChronoUnit.DAYS.between(LocalDate.now(), tarefa.getDataLimite());
+                if (diasRestantes <= 1) {
+                    System.out.println("Notificação: A tarefa '" + tarefa.getTitulo() + "' está próxima do prazo!");
+                }
+            }
+        });
+
+
+    }
+}
