86185ef097f2fdc0f275f3bcb768766b90d28f84
williamsza <wyllsza@gmail.com>
2025-03-18 06:01:21 -0300

    Criação da estrutura de pacotes

---
+++ src/test/java/br/com/todo/list/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.todo.list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
