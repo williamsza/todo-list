7f4c04f0dad7f76bf3c3b04cce3005a3a3ab40f0
williamsza <wyllsza@gmail.com>
2025-03-23 05:30:18 -0300

    Criado TarefaException

--- src/main/java/br/com/todo/list/domain/TarefaExcepiton.java
+++ src/main/java/br/com/todo/list/domain/TarefaExcepiton.java
@@ -1,6 +1,7 @@
 package br.com.todo.list.domain;
 
 public class TarefaExcepiton extends Exception {
-    public TarefaExcepiton(String s) {
+    public TarefaExcepiton(String message) {
+        super(message);
     }
 }
